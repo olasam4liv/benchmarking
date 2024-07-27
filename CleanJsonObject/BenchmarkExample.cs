0aa89caa162a629bbed6f2cc347352e10a283936
User <User@SAM>
2024-07-27 01:15:47 +0100

    Add project files.

---
+++ CleanJsonObject/BenchmarkExample.cs
@@ -0,0 +1,110 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    using System;
+using System.Data.SqlClient;
+
+
+namespace CleanJsonObject
+{
+    [MemoryDiagnoser(false)]
+    public class BenchmarkExample
+    {
+        [Benchmark]
+        public int FindMaxNumberMethodOne()
+        {
+            var numbers = new int [1000];
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = i;
+
+            var maxNumber = numbers.OrderByDescending(x => x).FirstOrDefault();
+
+            return maxNumber;
+        }
+        [Benchmark]
+        public int FindMaxNumberMethodTwo()
+        {
+            var numbers = new int[1000];
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = i;
+
+            var maxNumber = numbers.MaxBy(x => x);
+
+            return maxNumber;
+        }
+
+        [Benchmark]
+        public int FindMaxNumberMethodThree()
+        {
+            var numbers = new int[1000];
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = i;
+
+            var maxNumber = numbers.Max(x => x);
+
+            return maxNumber;
+        }
+
+
+
+
+
+
+
+        public async Task HandleFileStream()
+        {
+            // Correct way to handle file streams
+            using (FileStream fs = new FileStream("example.txt", FileMode.OpenOrCreate))
+            {
+                // Perform file operations
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.WriteLine("Hello, world!");
+                }
+            } // FileStream and StreamWriter are automatically disposed of here
+        }
+
+
+
+
+        
+
+
+    }
+     
+
+public class DatabaseExample
+{
+    public void FetchData()
+    {
+        string connectionString = "YourConnectionStringHere";
+        
+        // Using 'using' to ensure the connection is properly disposed of
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            
+            string query = "SELECT * FROM YourTable";
+            
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Column1: {reader[0]}, Column2: {reader[1]}");
+                    }
+                }
+            }
+        } // SqlConnection, SqlCommand, and SqlDataReader are automatically disposed of here
+    }
+}
+
+
+}
---
+++ CleanJsonObject/Todo.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanJsonObject
+{
+    internal class Todo
+    {
+
+        public Todo()
+        {
+                
+        }
+
+        public void SomeMethod()
+        {
+            var result = SomeAsyncMethod().Result; // Blocking call
+        }
+
+
+        public async Task SomeMethodAsync()
+        {
+            var result = await SomeAsyncMethod();
+        }
+
+
+        private async Task<dynamic?> SomeAsyncMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+//        BenchmarkExample benchmarkExample = new BenchmarkExample();
+//        Console.WriteLine("FindMaxNumber: {0}", benchmarkExample.FindMaxNumber());
+//Console.WriteLine("FindMaxNumberMethod: {0}", benchmarkExample.FindMaxNumberMethod());
+        //using (HttpClient client = new HttpClient())
+        //{
+        //    try
+        //    {
+        //        string url = "https://coderbyte.com/api/challenges/json/json-cleaning";
+        //        HttpResponseMessage response = await client.GetAsync(url);
+        //        response.EnsureSuccessStatusCode();
+
+        //        string jsonString = await response.Content.ReadAsStringAsync();
+        //        Console.WriteLine(jsonString);
+        //        JObject jsonObject = JObject.Parse(jsonString);
+
+        //        CleanJsonObject(jsonObject);
+
+        //        string cleanedJsonString = JsonConvert.SerializeObject(jsonObject);
+        //        Console.WriteLine(cleanedJsonString);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine($"An error occurred: {e.Message}");
+        //    }
+        //}
+
+        //static void CleanJsonObject(JToken token)
+        //{
+        //    if (token is JObject)
+        //    {
+        //        var propertiesToRemove = new List<JProperty>();
+
+        //        foreach (var property in token.Children<JProperty>())
+        //        {
+        //            if (property.Value.Type == JTokenType.String &&
+        //                (property.Value.ToString() == "N/A" || property.Value.ToString() == "-" || property.Value.ToString() == ""))
+        //            {
+        //                propertiesToRemove.Add(property);
+        //            }
+        //            else
+        //            {
+        //                CleanJsonObject(property.Value);
+        //            }
+        //        }
+
+        //        foreach (var property in propertiesToRemove)
+        //        {
+        //            property.Remove();
+        //        }
+        //    }
+        //    else if (token is JArray)
+        //    {
+        //        var itemsToRemove = new List<JToken>();
+
+        //        foreach (var item in token.Children())
+        //        {
+        //            if (item.Type == JTokenType.String &&
+        //                (item.ToString() == "N/A" || item.ToString() == "-" || item.ToString() == ""))
+        //            {
+        //                itemsToRemove.Add(item);
+        //            }
+        //            else
+        //            {
+        //                CleanJsonObject(item);
+        //            }
+        //        }
+
+        //        foreach (var item in itemsToRemove)
+        //        {
+        //            item.Remove();
+        //        }
+        //    }
+        //}
+
+    }
+}
